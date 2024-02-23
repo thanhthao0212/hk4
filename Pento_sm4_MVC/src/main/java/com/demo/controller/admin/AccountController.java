407222c35542914c2d5d26bcb4e0d439c8e389b8
LODNight <khuunhattin123@gmail.com>
2024-02-23 13:01:46 +0700

    Add Controller-Admin-Account

--- Pento_sm4_MVC/src/main/java/com/demo/controller/admin/AccountController.java
+++ Pento_sm4_MVC/src/main/java/com/demo/controller/admin/AccountController.java
@@ -12,12 +12,16 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.demo.entities.Account;
 import com.demo.entities.Role;
 import com.demo.service.AccountService;
 import com.demo.service.RoleService;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping({ "account", "account/"})
@@ -95,16 +99,73 @@ public class AccountController {
 		return "redirect:/account/index";
 	}
 	
+	//
+	@RequestMapping(value = "verify", method = RequestMethod.GET)
+	public String verify(@RequestParam("email") String email, @RequestParam("code") String code,
+			RedirectAttributes redirectAttributes) {
+		Account account = accountService.findByEmail(email);
+		if (account == null) {
+			redirectAttributes.addFlashAttribute("msg", "email not found");
+		} else {
+			if (code.equals(account.getSecurityCode())) {
+				account.setStatus(true);
+				if (accountService.save(account)) {
+					redirectAttributes.addFlashAttribute("msg", "Actived");
+				} else {
+					redirectAttributes.addFlashAttribute("msg", "Failed");
+				}
+			} else {
+				redirectAttributes.addFlashAttribute("msg", "Failed");
+			}
+		}
+		return "redirect:/account/login";
+	}
+	
+	@RequestMapping(value = "logout", method = RequestMethod.GET)
+	public String logout(HttpSession session) {
+		session.removeAttribute("email");
+		return "redirect:/account/login";
+	}
+
+	@RequestMapping(value = "forgetpassword", method = RequestMethod.GET)
+	public String forgetpassword() {
+		return "account/forgetpassword";
+	}
+
+	@RequestMapping(value = "forgetpassword", method = RequestMethod.POST)
+	public String forgetpassword(@RequestParam("email") String email, RedirectAttributes redirectAttributes) {
+		Account account = accountService.findByEmail(email);
+		if (account == null) {
+			redirectAttributes.addFlashAttribute("msg", "Email not found");
+			return "redirect:/account/forgetpassword";
+		} else {
+			String securityCode = SecurityCodeHelper.generate();
+			account.setSecurityCode(securityCode);
+			if (accountService.save(account)) {
+				String content = "Nhan vao <a href='http://localhost:8085/account/updatepassword?code=" + securityCode
+						+ "&email=" + account.getemail() + "'>day</a> de cap nhat password";
+				mailService.send(environment.getProperty("spring.mail.email"), account.getEmail(), "Update Password",
+						content);
+			}
+			return "redirect:/account/login";
+		}
+	}
+	
 	// Update Password
 	@GetMapping({"updatePassword"})
-	public String updatePassword(@PathVariable("email") String email, RedirectAttributes redirectAttributes, ModelMap modelMap) {
+	public String updatePassword(@RequestParam("email") String email, RedirectAttributes redirectAttributes, ModelMap modelMap) {
 		Account account = accountService.findByEmail(email);
-		if(account == null) {
-			redirectAttributes.addFlashAttribute("msg", "Username Not Found");
+		if (account == null) {
+			redirectAttributes.addFlashAttribute("msg", "Email not found");
 		} else {
-			return "account/updatePassword";			
+			if (code.equals(account.getSecurityCode())) {
+				modelMap.put("email", email);
+				return "account/updatepassword";
+			} else {
+				redirectAttributes.addFlashAttribute("msg", "Security Code is Invalid");
+			}
 		}
-		return "redirect:/account/index";
+		return "redirect:/account/login";
 	}
 	
 	@PostMapping({ "updatePassword" })
